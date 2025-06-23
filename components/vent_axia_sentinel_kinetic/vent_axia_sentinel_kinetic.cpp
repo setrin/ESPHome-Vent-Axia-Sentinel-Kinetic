72daa043d0f57a180a6bed091f919d3067650937
alextrical <35117191+alextrical@users.noreply.github.com>
2025-06-23 21:32:00 +0100

    Simplified External component, and moved most code to YAML

--- components/vent_axia_sentinel_kinetic/vent_axia_sentinel_kinetic.cpp
+++ components/vent_axia_sentinel_kinetic/vent_axia_sentinel_kinetic.cpp
@@ -7,8 +7,7 @@
 namespace esphome {
   namespace vent_axia_sentinel_kinetic {
 
-    static
-    const char * TAG = "vent_axia_sentinel_kinetic.component";
+    static const char * TAG = "vent_axia_sentinel_kinetic.component";
 
     VentAxiaSentinelKineticComponent* VentAxiaSentinelKineticComponent::instance = nullptr;
 
@@ -49,48 +48,6 @@ namespace esphome {
     }
 
     void VentAxiaSentinelKineticComponent::loop() {
-      int32_t now = millis();
-      if (this->get_diagnostic) {
-        if (!this->diagnostic_time_set_) {
-          this->diagnostic_time_ = now;
-          this->toggle_time_ = now;
-          this->diagnostic_time_set_ = true;
-          this->got_diagnostic_ = false;
-        }
-        if (now - this->diagnostic_time_ <= 60000){ //timeout incase we don't enter diagnostic
-          if (id(line1_).state.rfind("Diagnostic", 0) != 0 && !got_diagnostic_){ //We arent in the Diagnostic Menu, Hold Up and Main until we are
-            if (CMD_KEY_DATA != 0x0A) CMD_KEY_DATA = 0x0A; //Up+Main
-          } else if (!this->got_diagnostic_){ //Scroll down and Fetch all Diagnostic data
-            if (now - this->toggle_time_ > 50) {
-              if (CMD_KEY_DATA != 0x00){ //toggle
-                CMD_KEY_DATA = 0x01; //Down
-              } else {
-                CMD_KEY_DATA = 0x00; //Release all
-              }
-              this->toggle_time_ = now;
-            }
-            if (id(line1_).state.rfind("Diagnostic  28", 0) == 0) this->got_diagnostic_ = true; //We are at the bottom entry od the Diagnostic menu, we have seen everything
-          } else if (id(line1_).state.rfind("Diagnostic", 0) == 0 && this->got_diagnostic_){ //We have all Diagnostic data we want, Now exit
-                if (now - this->toggle_time_ > 1000) {
-                  CMD_KEY_DATA = 0x00; //Release all
-                  this->toggle_time_ = now;
-                } else if (CMD_KEY_DATA != 0x02) {
-                  CMD_KEY_DATA = 0x02; //Up
-                }
-          } else { //We have exitetd the diagnostic menu, release all keys 
-            if (CMD_KEY_DATA != 0x00) CMD_KEY_DATA = 0x00; //Release all
-            this->get_diagnostic = false;
-            this->diagnostic_time_set_ = false;
-            this->got_diagnostic_ = false;
-          }
-        } else if (this->get_diagnostic) { //We timed out, release all keys
-            if (CMD_KEY_DATA != 0x00) CMD_KEY_DATA = 0x00; //Release all
-            this->get_diagnostic = false;
-            this->diagnostic_time_set_ = false;
-            this->got_diagnostic_ = false;
-        }
-      }
-
       //Send serial packets
       if (CMD_KEY_DATA != 0) {
         if (CMD_KEY_DATA != LAST_CMD_KEY_DATA_) {
@@ -128,9 +85,9 @@ namespace esphome {
           return; // Wait for header
         }
 
-        this->buffer[this->current_index_++] = c;
+        this->buffer_[this->current_index_++] = c;
 
-        if (this->current_index_ == sizeof(this->buffer)) {
+        if (this->current_index_ == sizeof(this->buffer_)) {
           if (this->validate_crc_()) {
             // this->packet_ready_ = true;
             this->process_packet_();
@@ -138,7 +95,6 @@ namespace esphome {
           this->current_index_ = 0;
         }
       }
-
     }
 
     void VentAxiaSentinelKineticComponent::dump_config() {
@@ -162,35 +118,35 @@ namespace esphome {
     }
 
     void VentAxiaSentinelKineticComponent::calculate_command_(const uint8_t * command_value, uint8_t command) {
-      cmdbuffer_[0] = CMD_FRAME_HEADER;
+      this->cmdbuffer_[0] = CMD_FRAME_HEADER;
       // command value bytes
       if (command_value != nullptr) {
         for (int i = 0; i < 4; i++) {
-          cmdbuffer_[i + 1] = command_value[i];
+          this->cmdbuffer_[i + 1] = command_value[i];
         }
       }
-      cmdbuffer_[5] = command;
+      this->cmdbuffer_[5] = command;
 
       uint16_t crc = 0xFFFF;
       for (uint32_t i = 0; i < 6; i++) {
-        crc -= cmdbuffer_[i];
+        crc -= this->cmdbuffer_[i];
       }
-      cmdbuffer_[6] = highbyte(crc);
-      cmdbuffer_[7] = lowbyte(crc);
+      this->cmdbuffer_[6] = highbyte(crc);
+      this->cmdbuffer_[7] = lowbyte(crc);
       send_command_();
     }
 
     void VentAxiaSentinelKineticComponent::send_command_() {
-      this->write_array(cmdbuffer_, 8);
+      this->write_array(this->cmdbuffer_, 8);
     }
 
     bool VentAxiaSentinelKineticComponent::validate_crc_() {
       uint16_t crc = 0xFFFF;
       for (int i = 0; i < 39; i++) {
-        crc -= buffer[i];
+        crc -= this->buffer_[i];
       }
 
-      uint16_t received_crc = (buffer[39] << 8) | buffer[40];
+      uint16_t received_crc = (this->buffer_[39] << 8) | this->buffer_[40];
       return (crc == received_crc);
     }
 
@@ -207,211 +163,24 @@ namespace esphome {
       CMD_KEY_DATA = (CMD_KEY_DATA & ~(1 << 3)) | (enable << 3);
     }
 
-    void VentAxiaSentinelKineticComponent::publishDiagnosticIfValid(text_sensor::TextSensor* diag, const std::string& buff) {
-      if (diag != nullptr) {
-          diag->publish_state(buff.substr(18, 16).c_str());
-      }
-    }
-
     void VentAxiaSentinelKineticComponent::process_packet_() {
-      if (std::memcmp(buffer, last_buffer, sizeof(buffer)) != 0){ //Only process the string if its changed
-        std::string buff(reinterpret_cast < const char * > (buffer + 5), 34);
-        this->line1_ -> publish_state(buff.substr(1, 16).c_str());
-        this->line2_ -> publish_state(buff.substr(18, 16).c_str());
-
-        if (buff.substr(1, 10) == "Diagnostic") {
-          int code = std::stoi(buff.substr(13, 2)); // Convert to integer
-
-          switch (code) {
-          case 0: publishDiagnosticIfValid(this->diagnostic0_, buff); break;
-          case 1: publishDiagnosticIfValid(this->diagnostic1_, buff); break;
-          case 2: publishDiagnosticIfValid(this->diagnostic2_, buff); break;
-          case 3: publishDiagnosticIfValid(this->diagnostic3_, buff); break;
-          case 4: publishDiagnosticIfValid(this->diagnostic4_, buff); break;
-          case 5: publishDiagnosticIfValid(this->diagnostic5_, buff); break;
-          case 6: publishDiagnosticIfValid(this->diagnostic6_, buff); break;
-          case 7: publishDiagnosticIfValid(this->diagnostic7_, buff); break;
-          case 8: publishDiagnosticIfValid(this->diagnostic8_, buff); break;
-          case 9: publishDiagnosticIfValid(this->diagnostic9_, buff); break;
-          case 10: publishDiagnosticIfValid(this->diagnostic10_, buff); break;
-          case 11: publishDiagnosticIfValid(this->diagnostic11_, buff); break;
-          case 12: publishDiagnosticIfValid(this->diagnostic12_, buff); break;
-          case 13: publishDiagnosticIfValid(this->diagnostic13_, buff); break;
-          case 14: publishDiagnosticIfValid(this->diagnostic14_, buff); break;
-          case 15: publishDiagnosticIfValid(this->diagnostic15_, buff); break;
-          case 16: publishDiagnosticIfValid(this->diagnostic16_, buff); break;
-          case 17: publishDiagnosticIfValid(this->diagnostic17_, buff); break;
-          case 18: publishDiagnosticIfValid(this->diagnostic18_, buff); break;
-          case 19: publishDiagnosticIfValid(this->diagnostic19_, buff); break;
-          case 20: publishDiagnosticIfValid(this->diagnostic20_, buff); break;
-          case 21: publishDiagnosticIfValid(this->diagnostic21_, buff); break;
-          case 22: publishDiagnosticIfValid(this->diagnostic22_, buff); break;
-          case 23: publishDiagnosticIfValid(this->diagnostic23_, buff); break;
-          case 24: publishDiagnosticIfValid(this->diagnostic24_, buff); break;
-          case 25: publishDiagnosticIfValid(this->diagnostic25_, buff); break;
-          case 26: publishDiagnosticIfValid(this->diagnostic26_, buff); break;
-          case 27: publishDiagnosticIfValid(this->diagnostic27_, buff); break;
-          case 28: publishDiagnosticIfValid(this->diagnostic28_, buff); break;
-          // case 0:
-          //   if (this->diagnostic0_ != nullptr) {
-          //     this->diagnostic0_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 1:
-          //   if (this->diagnostic1_ != nullptr) {
-          //     this->diagnostic1_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 2:
-          //   if (this->diagnostic2_ != nullptr) {
-          //     this->diagnostic2_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 3:
-          //   if (this->diagnostic3_ != nullptr) {
-          //     this->diagnostic3_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 4:
-          //   if (this->diagnostic4_ != nullptr) {
-          //     this->diagnostic4_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 5:
-          //   if (this->diagnostic5_ != nullptr) {
-          //     this->diagnostic5_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 6:
-          //   if (this->diagnostic6_ != nullptr) {
-          //     this->diagnostic6_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 7:
-          //   if (this->diagnostic7_ != nullptr) {
-          //     this->diagnostic7_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 8:
-          //   if (this->diagnostic8_ != nullptr) {
-          //     this->diagnostic8_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 9:
-          //   if (this->diagnostic9_ != nullptr) {
-          //     this->diagnostic9_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 10:
-          //   if (this->diagnostic10_ != nullptr) {
-          //     this->diagnostic10_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 11:
-          //   if (this->diagnostic11_ != nullptr) {
-          //     this->diagnostic11_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 12:
-          //   if (this->diagnostic12_ != nullptr) {
-          //     this->diagnostic12_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 13:
-          //   if (this->diagnostic13_ != nullptr) {
-          //     this->diagnostic13_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 14:
-          //   if (this->diagnostic14_ != nullptr) {
-          //     this->diagnostic14_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 15:
-          //   if (this->diagnostic15_ != nullptr) {
-          //     this->diagnostic15_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 16:
-          //   if (this->diagnostic16_ != nullptr) {
-          //     this->diagnostic16_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 17:
-          //   if (this->diagnostic17_ != nullptr) {
-          //     this->diagnostic17_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 18:
-          //   if (this->diagnostic18_ != nullptr) {
-          //     this->diagnostic18_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 19:
-          //   if (this->diagnostic19_ != nullptr) {
-          //     this->diagnostic19_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 20:
-          //   if (this->diagnostic20_ != nullptr) {
-          //     this->diagnostic20_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 21:
-          //   if (this->diagnostic21_ != nullptr) {
-          //     this->diagnostic21_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 22:
-          //   if (this->diagnostic22_ != nullptr) {
-          //     this->diagnostic22_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 23:
-          //   if (this->diagnostic23_ != nullptr) {
-          //     this->diagnostic23_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 24:
-          //   if (this->diagnostic24_ != nullptr) {
-          //     this->diagnostic24_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 25:
-          //   if (this->diagnostic25_ != nullptr) {
-          //     this->diagnostic25_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 26:
-          //   if (this->diagnostic26_ != nullptr) {
-          //     this->diagnostic26_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 27:
-          //   if (this->diagnostic27_ != nullptr) {
-          //     this->diagnostic27_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          // case 28:
-          //   if (this->diagnostic28_ != nullptr) {
-          //     this->diagnostic28_ -> publish_state(buff.substr(18, 16).c_str());
-          //   }
-          //   break;
-          default:
-            break;
-          }
+      if (std::memcmp(this->buffer_, this->last_buffer_, sizeof(this->buffer_)) != 0){ //Only process the string if its changed
+        std::string buff(reinterpret_cast < const char * > (this->buffer_ + 5), 34);
+        
+        std::string new_line1_state = buff.substr(1, 16);
+        if (new_line1_state != this->prev_line1_state_) {
+            this->line1_->publish_state(new_line1_state.c_str());
+            this->prev_line1_state_ = new_line1_state;
         }
-        // last_buffer = buffer;
-        std::memcpy(this->last_buffer, this->buffer, sizeof(buffer));
+
+        std::string new_line2_state = buff.substr(18, 16);
+        if (new_line2_state != this->prev_line2_state_) {
+            this->line2_->publish_state(new_line2_state.c_str());
+            this->prev_line2_state_ = new_line2_state;
+        }
+
+        std::memcpy(this->last_buffer_, this->buffer_, sizeof(this->buffer_));
       }
-    }
-
-    void DiagnosticButton::dump_config() {
-      LOG_BUTTON("", "Diagnostic Button", this);
-    }
-
-    void DiagnosticButton::press_action() {
-      this->parent_ -> get_diagnostic = true;
     }
 
   } // namespace vent_axia_sentinel_kinetic
--- components/vent_axia_sentinel_kinetic/vent_axia_sentinel_kinetic.h
+++ components/vent_axia_sentinel_kinetic/vent_axia_sentinel_kinetic.h
@@ -55,75 +55,13 @@ namespace esphome {
 
         void set_line1(text_sensor::TextSensor * text_sensor) {line1_ = text_sensor;}
         void set_line2(text_sensor::TextSensor * text_sensor) {line2_ = text_sensor;}
-        void set_diagnostic0(text_sensor::TextSensor * text_sensor) {diagnostic0_ = text_sensor;}
-        void set_diagnostic1(text_sensor::TextSensor * text_sensor) {diagnostic1_ = text_sensor;}
-        void set_diagnostic2(text_sensor::TextSensor * text_sensor) {diagnostic2_ = text_sensor;}
-        void set_diagnostic3(text_sensor::TextSensor * text_sensor) {diagnostic3_ = text_sensor;}
-        void set_diagnostic4(text_sensor::TextSensor * text_sensor) {diagnostic4_ = text_sensor;}
-        void set_diagnostic5(text_sensor::TextSensor * text_sensor) {diagnostic5_ = text_sensor;}
-        void set_diagnostic6(text_sensor::TextSensor * text_sensor) {diagnostic6_ = text_sensor;}
-        void set_diagnostic7(text_sensor::TextSensor * text_sensor) {diagnostic7_ = text_sensor;}
-        void set_diagnostic8(text_sensor::TextSensor * text_sensor) {diagnostic8_ = text_sensor;}
-        void set_diagnostic9(text_sensor::TextSensor * text_sensor) {diagnostic9_ = text_sensor;}
-        void set_diagnostic10(text_sensor::TextSensor * text_sensor) {diagnostic10_ = text_sensor;}
-        void set_diagnostic11(text_sensor::TextSensor * text_sensor) {diagnostic11_ = text_sensor;}
-        void set_diagnostic12(text_sensor::TextSensor * text_sensor) {diagnostic12_ = text_sensor;}
-        void set_diagnostic13(text_sensor::TextSensor * text_sensor) {diagnostic13_ = text_sensor;}
-        void set_diagnostic14(text_sensor::TextSensor * text_sensor) {diagnostic14_ = text_sensor;}
-        void set_diagnostic15(text_sensor::TextSensor * text_sensor) {diagnostic15_ = text_sensor;}
-        void set_diagnostic16(text_sensor::TextSensor * text_sensor) {diagnostic16_ = text_sensor;}
-        void set_diagnostic17(text_sensor::TextSensor * text_sensor) {diagnostic17_ = text_sensor;}
-        void set_diagnostic18(text_sensor::TextSensor * text_sensor) {diagnostic18_ = text_sensor;}
-        void set_diagnostic19(text_sensor::TextSensor * text_sensor) {diagnostic19_ = text_sensor;}
-        void set_diagnostic20(text_sensor::TextSensor * text_sensor) {diagnostic20_ = text_sensor;}
-        void set_diagnostic21(text_sensor::TextSensor * text_sensor) {diagnostic21_ = text_sensor;}
-        void set_diagnostic22(text_sensor::TextSensor * text_sensor) {diagnostic22_ = text_sensor;}
-        void set_diagnostic23(text_sensor::TextSensor * text_sensor) {diagnostic23_ = text_sensor;}
-        void set_diagnostic24(text_sensor::TextSensor * text_sensor) {diagnostic24_ = text_sensor;}
-        void set_diagnostic25(text_sensor::TextSensor * text_sensor) {diagnostic25_ = text_sensor;}
-        void set_diagnostic26(text_sensor::TextSensor * text_sensor) {diagnostic26_ = text_sensor;}
-        void set_diagnostic27(text_sensor::TextSensor * text_sensor) {diagnostic27_ = text_sensor;}
-        void set_diagnostic28(text_sensor::TextSensor * text_sensor) {diagnostic28_ = text_sensor;}
 
-        uint8_t buffer[41];
-        uint8_t last_buffer[41];
-        bool get_diagnostic = false;
 
       protected:
         text_sensor::TextSensor * line1_ {nullptr};
         text_sensor::TextSensor * line2_ {nullptr};
-        text_sensor::TextSensor * diagnostic0_ {nullptr};
-        text_sensor::TextSensor * diagnostic1_ {nullptr};
-        text_sensor::TextSensor * diagnostic2_ {nullptr};
-        text_sensor::TextSensor * diagnostic3_ {nullptr};
-        text_sensor::TextSensor * diagnostic4_ {nullptr};
-        text_sensor::TextSensor * diagnostic5_ {nullptr};
-        text_sensor::TextSensor * diagnostic6_ {nullptr};
-        text_sensor::TextSensor * diagnostic7_ {nullptr};
-        text_sensor::TextSensor * diagnostic8_ {nullptr};
-        text_sensor::TextSensor * diagnostic9_ {nullptr};
-        text_sensor::TextSensor * diagnostic10_ {nullptr};
-        text_sensor::TextSensor * diagnostic11_ {nullptr};
-        text_sensor::TextSensor * diagnostic12_ {nullptr};
-        text_sensor::TextSensor * diagnostic13_ {nullptr};
-        text_sensor::TextSensor * diagnostic14_ {nullptr};
-        text_sensor::TextSensor * diagnostic15_ {nullptr};
-        text_sensor::TextSensor * diagnostic16_ {nullptr};
-        text_sensor::TextSensor * diagnostic17_ {nullptr};
-        text_sensor::TextSensor * diagnostic18_ {nullptr};
-        text_sensor::TextSensor * diagnostic19_ {nullptr};
-        text_sensor::TextSensor * diagnostic20_ {nullptr};
-        text_sensor::TextSensor * diagnostic21_ {nullptr};
-        text_sensor::TextSensor * diagnostic22_ {nullptr};
-        text_sensor::TextSensor * diagnostic23_ {nullptr};
-        text_sensor::TextSensor * diagnostic24_ {nullptr};
-        text_sensor::TextSensor * diagnostic25_ {nullptr};
-        text_sensor::TextSensor * diagnostic26_ {nullptr};
-        text_sensor::TextSensor * diagnostic27_ {nullptr};
-        text_sensor::TextSensor * diagnostic28_ {nullptr};
         void calculate_command_(const uint8_t * command_value, uint8_t command_str);
         void send_alive_str_();
-        void publishDiagnosticIfValid(text_sensor::TextSensor* diag, const std::string& buff);
 
         void send_command_();
         int32_t last_periodic_millis_ = millis();
@@ -132,12 +70,13 @@ namespace esphome {
         uint8_t current_index_ = 0;
         uint8_t cmdbuffer_[8];
         uint8_t LAST_CMD_KEY_DATA_ = 0x00;
-        // bool packet_ready_ = false;
 
         int32_t toggle_time_;
-        bool diagnostic_time_set_ = false;
-        int32_t diagnostic_time_;
-        bool got_diagnostic_ = false;
+
+        uint8_t buffer_[41];
+        uint8_t last_buffer_[41];
+        std::string prev_line1_state_;
+        std::string prev_line2_state_;
 
         static VentAxiaSentinelKineticComponent* instance;
         void IRAM_ATTR timer_isr();
@@ -146,15 +85,6 @@ namespace esphome {
         hw_timer_t *timer = nullptr;
 #endif
     };
-
-class DiagnosticButton: public Component, public button::Button {
- public:
-  void dump_config() override;
-  void set_parent(VentAxiaSentinelKineticComponent *parent) { this->parent_ = parent; }
- protected:
-  void press_action() override;
-  VentAxiaSentinelKineticComponent *parent_;
-};
 
   } // namespace vent_axia_sentinel_kinetic
 } // namespace esphome
